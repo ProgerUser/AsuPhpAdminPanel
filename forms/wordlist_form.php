9462be5dd6a56d16935469815f16f97fa0b37bc0
ProgerUser <said.pachuliya@mail.ru>
2022-11-20 22:56:27 +0300

    Исправление неверной переменной

--- forms/wordlist_form.php
+++ forms/wordlist_form.php
@@ -2,21 +2,21 @@
     <div class="form-group">
         <label for="word">Слово *</label>
         <input type="text" name="word"
-               value="<?php echo htmlspecialchars(($edit ? $dictlist['word'] : '') ?: '', ENT_QUOTES, 'UTF-8'); ?>"
+               value="<?php echo htmlspecialchars(($edit ? $wordlist['word'] : '') ?: '', ENT_QUOTES, 'UTF-8'); ?>"
                placeholder="Слово" class="form-control" required="required" id="word">
     </div>
 
     <div class="form-group">
         <label for="translate">Перевод *</label>
         <input type="text" name="translate"
-               value="<?php echo htmlspecialchars(($edit ? $dictlist['translate'] : '') ?: '', ENT_QUOTES, 'UTF-8'); ?>"
+               value="<?php echo htmlspecialchars(($edit ? $wordlist['translate'] : '') ?: '', ENT_QUOTES, 'UTF-8'); ?>"
                placeholder="Перевод" class="form-control" required="required" id="translate">
     </div>
 
     <div class="form-group">
         <label for="dict_ref">Ссылка на словарь</label>
         <textarea name="dict_ref" placeholder="Ссылка на словарь" class="form-control"
-                  id="dict_ref"><?php echo htmlspecialchars((($edit) ? $dictlist['dict_ref'] : '') ?: '', ENT_QUOTES, 'UTF-8'); ?></textarea>
+                  id="dict_ref"><?php echo htmlspecialchars((($edit) ? $wordlist['dict_ref'] : '') ?: '', ENT_QUOTES, 'UTF-8'); ?></textarea>
     </div>
 
     <div class="form-group text-center">
